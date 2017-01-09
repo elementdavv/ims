bac23d9afbbcfb3d9415fd587fd4629bbbbafcaf
zhu_jq <zhu_jq@pataw.cn>
2017-01-09 16:25:05 +0800

    更新
    Signed-off-by: zhu_jq <zhu_jq@pataw.cn>

--- sealtalk/WebRoot/page/web/js/main.js
+++ sealtalk/WebRoot/page/web/js/main.js
@@ -2,11 +2,109 @@
  * Created by zhu_jq on 2017/1/4.
  */
 window.onload = function() {
+
+    $('.newsTabContent').bind('contextmenu',function(){
+        event.preventDefault();
+        return false;
+    });
+
+
+
+    //群组右键
+    $('body').delegate('#groupLeftClick li','click',function(){
+        $('.myContextMenu').remove();
+        var index = $(this).closest('ul').find('li').index($(this));
+        switch (index)
+        {
+            case 0:
+                console.log(0);
+                break;
+            case 1:
+                console.log(1);
+                break;
+            case 2:
+                console.log(2);
+                break;
+            case 3:
+                //解散群
+                new Window().alert({
+                    title   : '解散群',
+                    content : '确定要解散群么？',
+                    hasCloseBtn : true,
+                    hasImg : true,
+                    textForSureBtn : '确定',              //确定按钮
+                    textForcancleBtn : '取消',            //取消按钮
+                    handlerForCancle : null,
+                    //handlerForClose : null,
+                    handlerForSure : null
+                });
+                break;
+            case 4:
+                console.log(4);
+                break;
+
+        }
+    })
+    $('.newsTabContent').delegate('.groupChatListUl li','mousedown',function(e){
+
+        if(e.buttons==2){
+            var left = e.clientX;
+            var top = e.clientY;
+            var arr = ['发起群聊','查看群资料','群成员管理','解散群','转让群']
+            var style = 'left:'+left+'px;top:'+top+'px';
+            var id = 'groupLeftClick'
+            fshowContexMenu(arr,style,id)
+        }
+        $('.groupChatListUl li').removeClass('active');
+        $(this).addClass('active');
+        return false;
+    })
+
+
+
+
+    $('.seeOrgnizeTree').click(function(){
+        seeOrgnizeTree();
+    })
+
+
+
+
+    $('.organizationList').delegate('ul li .groupCollspan','click',function(e){
+
+
+        //按钮样式
+        var $groupCollspanO = $(this)
+        var bOpen = $groupCollspanO.hasClass('groupCollspanO')
+        if(bOpen){
+            $groupCollspanO.removeClass('groupCollspanO');
+            $groupCollspanO.addClass('groupCollspanC');
+        }else{
+            $groupCollspanO.removeClass('groupCollspanC');
+            $groupCollspanO.addClass('groupCollspanO');
+        }
+        //内容显示隐藏
+        $(this).closest('li').next('ul').slideToggle();
+    })
+
+
     /*顶部&&左侧导航切换*/
     $('.chatHeaderMenu,.chatMenu').click(function (e) {
         $(e.target).addClass('active')
         $(e.target).siblings('li').removeClass('active');
         var nShowClass = $(e.target).attr('bindPanel');
+        switch(nShowClass)
+        {
+            case 'news':
+                $('.chatBox').html('aaaaa')
+                break;
+            case 'orgnized':
+                $('.chatBox').html('bbbb')
+                break;
+            case 'back':
+                $('.chatBox').html('cccc')
+                break;
+        }
         nShowClass&&showPanel(nShowClass);
     })
 
@@ -30,6 +128,21 @@ window.onload = function() {
         $(this).find('.operMenuList').slideToggle();
     })
 
+}
+
+
+
+function fshowContexMenu(arr,style,id){
+
+    var listHTML = '';
+    for(var i = 0;i<arr.length;i++){
+        listHTML+='<li>'+arr[i]+'</li>'
+    }
+    var sHTML = '<div class="myContextMenu" id="'+id+'" style="'+style+'">'+
+        '<div class="contextTri"></div>'+
+        '<ul>'+listHTML+'</ul>'+
+        '</div>';
+    $('body').append($(sHTML));
 }
 function showPanel(panelClass){
     var eShowNode = $("."+panelClass);
--- sealtalk/WebRoot/page/web/js/signin.js
+++ sealtalk/WebRoot/page/web/js/signin.js
@@ -2,102 +2,15 @@
  * Created by zhu_jq on 2017/1/4.
  */
 window.onload = function(){
-
-    //点击发送验证码
-    $('.SendCheakCode').click(function(){
-        fSendCheakCode();
-        var phoneNum = $('#phoneNum').val();
-        var data = JSON.stringify({phoneNum:phoneNum});
-        sendAjax('system!requestText',data,function(){
-            console.log('验证码发送成功');
-
-        })
+    $('.prevfun').click(function(){
+        console.log(111);
     })
+
 }
-
-
-/*
-* 通用AJAX
-*/
-function sendAjax(url,data,callback){
-    $.ajax({
-        type: "POST",
-        url: url,
-        data:data,
-        success: function(data){
-            callback && callback(data);
-        }
-    })
-}
-
-
-/*
-*
-* 跳转到第二部
-* */
-function fToStep2(dom){
-    var phoneNum = $('#username').val();
-    var textcode = $('#checkCode').val();
-    var data = JSON.stringify({'phone':phoneNum,textcode:textcode});
-    $('.sealtalk-forgetpassword').attr('account',phoneNum);
-    sendAjax('system!testText',data,function(){
-        fToNext(dom)
-    });
-}
-function fToStep3(dom){
-    var newpwd = $('#newpassword').val();
-    var comparepwd = $('#newpasswordCertain').val();
-    var account = $('.sealtalk-forgetpassword').attr('account');
-    var data = JSON.stringify({'newpwd':newpwd,comparepwd:comparepwd})
-    sendAjax('system!newPassword',data,function(){
-        fToNext(dom)
-    });
-}
-
-
-/*
-*
-* 发送验证码
-*
-*/
-function fSendCheakCode(){
-    var phoneNum = $('#username').val();
-    var data = JSON.stringify({'phoneNum':phoneNum})
-    sendAjax('system!requestText',data);
-}
-
-/*
-*
-* 登录
-*
-*/
-function signin(){
-    var accout = $('#username').val();
-    var userpwd = hex_md5($('#pwdIn').val());
-    var data = {'account':accout,'userpwd':userpwd};
-    //验证
-    sendAjax('system!afterLogin',data,function(data){
-        var datas = JSON.parse(data);
-        if(datas &&	datas.code == 1){
-           window.location.href = 'page/web/main.jsp';
-        }
-    });
-}
-
-/*
-*
-*跳转到登录页面
-*
-*/
 function fBackToSignin(){
-    window.location.href = 'system!login';
+    window.location.href = 'signin.html';
 }
 
-/*
-*
-*跳转到下一页处理
-*
-*/
 function fToNext(dom){
     console.log(dom);
     var sStep = $(dom).closest('.form-inline').attr('step');
@@ -108,12 +21,6 @@ function fToNext(dom){
         i>=4?fBackToSignin():fToNextStep(i);
     }
 }
-
-/*
- *
- *跳转到上一页处理
- *
- */
 function fToPrev(dom){
     console.log(dom);
     var sStep = $(dom).closest('.form-inline').attr('step');
@@ -124,12 +31,6 @@ function fToPrev(dom){
         i<=0?fBackToSignin():fToNextStep(i);
     }
 }
-
-/*
- *
- *跳转
- *
- */
 function fToNextStep(i){
     $('.resetStep').addClass('chatHide');
     $('[step=resetStep-'+i+']').closest('.resetStep').removeClass('chatHide');
