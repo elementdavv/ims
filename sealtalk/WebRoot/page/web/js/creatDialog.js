1f6aafc8507268ec025a0d68dc674001705fb6d8
zhu_jq <zhu_jq@pataw.cn>
2017-01-10 10:17:49 +0800

    弹窗
    Signed-off-by: zhu_jq <zhu_jq@pataw.cn>

---
+++ sealtalk/WebRoot/page/web/js/creatDialog.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhu_jq on 2017/1/9.
+ */
+//window.onload = function(){
+//    $('.dialogClose,.manageCancle').click(function(){
+//        $('.WindowMask').hide();
+//    });
+//}
+
--- sealtalk/WebRoot/page/web/js/rongyun.js
+++ sealtalk/WebRoot/page/web/js/rongyun.js
@@ -1,10 +1,7 @@
 /**
  * Created by zhu_jq on 2017/1/9.
  */
-
 $(function(){
-    //alert(1);
-})
-function creatGroupWindow(data){
+    RongIMClient.init("uwd1c0sxuwmx1");
 
-}
+})
