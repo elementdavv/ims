b8a68049bf50d68f75824ef80318f4386ee8dce9
zhu_jq <zhu_jq@pataw.cn>
2017-02-10 18:55:48 +0800

    xiazai
    Signed-off-by: zhu_jq <zhu_jq@pataw.cn>

--- sealtalk/WebRoot/page/web/js/fileDownload.js
+++ sealtalk/WebRoot/page/web/js/fileDownload.js
@@ -30,3 +30,41 @@ $(function(){
 function chDownloadProgress(){
     console.log('filedownload');
 }
+
+
+function chDownloadProgress(url, state, progress){
+    if (state == 'progressing') {
+        console.log(url, state, progress);
+        var fileName = url.split('attname=')[1];
+        var file = fileName.split('.')[0];
+        var targetA = $("a[fileName=" + file + "]");
+        var targetParent = targetA.parents('.mr-ownChat');
+        if ($('#down_process[uniquetime=' + file + ']').length == 0) {
+            $('#down_process[uniquetime=' + file + ']').remove();
+            var sHTML = '<div id="down_process" uniquetime="' + file + '">' +
+                '<div id="down_precent" uniquetime="' + file + '" style="width: 0%;">' +
+                '</div>' +
+                '</div>'
+            targetParent.append(sHTML);
+        } else {
+            $('#down_process[uniquetime=' + file + ']').find('#down_precent').css('width', '100%');
+        }
+    }
+    console.log(targetA);
+}
+
+
+function chDownloadState(url, state){
+    console.log(state);
+    if (state == 'completed') {
+        var fileName = url.split('attname=')[1];
+        var file = fileName.split('.')[0];
+        var targetA = $("a[fileName=" + file + "]");
+        var targetParent = targetA.parents('.mr-ownChat');
+        $('#down_process[uniquetime=' + file + ']').remove();
+        var sHTML = '<div id="fileOperate" uniquetime="1486626340273">' +
+            '<span class="openFile">打开文件</span><span class="openFloder">打开文件夹</span>' +
+            '</div>';
+        targetParent.append(sHTML);
+    }
+}
