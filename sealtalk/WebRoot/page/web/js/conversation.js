5c47efef6065499678bd7fdac0654a1b51b8bc13
gao_yn <gao_yn@pataw.cn>
2017-02-22 11:23:43 +0800

    提交 Signed-off-by: gao_yn <gao_yn@pataw.cn>

--- sealtalk/WebRoot/page/web/js/conversation.js
+++ sealtalk/WebRoot/page/web/js/conversation.js
@@ -365,15 +365,13 @@ function fillGroupPage(targetID,targetType,groupName){
             //$('#groupContainer .mr-chatview').empty();
             $('#groupContainer .mr-chateditBox').before(sDoM);
             var eDom=document.querySelector('#groupContainer .mr-chatview');
-            eDom.scrollTop = eDom.scrollHeight;
             if(eDom.scrollHeight>$('#groupContainer .mr-chatview').height()){
                 if($('#groupContainer .uploadImgFile').length!=0){
                     $('.uploadImgFile').on('load',function(){
                         eDom.scrollTop = eDom.scrollHeight;
                     })
-                }else{
-                    eDom.scrollTop = eDom.scrollHeight;
                 }
+                eDom.scrollTop = eDom.scrollHeight;
             }
             var $container = $('#groupContainer .mr-chatview');
             var eDom=document.querySelector('#groupContainer .mr-chatview');
